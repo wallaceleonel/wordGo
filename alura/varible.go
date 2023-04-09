247f07a16f3dc634d204d78ba0e914ff8f4a6d60
wallaceleonel <wallace.leonell@outlook.com>
2023-04-09 01:13:20 -0300

    refactor: formata classe

--- alura/varible.go
+++ alura/varible.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Gonewson"
-	var idade int = 9
-	var versao float32 = 1.1
+	nome := "Gonewson"
+	idade := 9
+	versao := 1.1
 
 	fmt.Println("ola sr.", nome, "cujo idade é ", idade)
 	fmt.Println("Esse programa está na versão.", versao)
