f4b0da240bf85276de331a1a2689a39d12fdae85
wallaceleonel <wallace.leonell@outlook.com>
2023-04-10 19:00:08 -0300

    feat: adicionando contas de formas diferentes.

--- projetos/bank/main.go
+++ projetos/bank/main.go
@@ -16,9 +16,21 @@ func main() {
 		numeroAgencia:       1313,
 		numeroContaCorrente: 12344566,
 		saldoConta:          12231.98}
-	fmt.Println(contaDoGoberto)
 
 	contaDoGo := contaCorrente{
-		"Go", 1313, 1231344, 212.98}
+		"Go",
+		1313,
+		1231344,
+		212.98}
+
+	var contaDoJones *contaCorrente
+	contaDoJones = new(contaCorrente)
+	contaDoJones.titular = "Jones Manoel"
+	contaDoJones.numeroAgencia = 1313
+	contaDoJones.numeroContaCorrente = 21211212
+	contaDoJones.saldoConta = 131313.13
+
+	fmt.Println(*contaDoJones)
+	fmt.Println(contaDoGoberto)
 	fmt.Println(contaDoGo)
 }
