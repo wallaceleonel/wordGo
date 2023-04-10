993846db0c1453c7ed159ac57cf174ac1a377428
wallaceleonel <wallace.leonell@outlook.com>
2023-04-10 08:43:31 -0300

    feat: aula 1.0.3

--- alura/varible.go
+++ alura/varible.go
@@ -9,4 +9,13 @@ func main() {
 
 	fmt.Println("ola sr.", nome, "cujo idade é ", idade)
 	fmt.Println("Esse programa está na versão.", versao)
+
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi ", comando)
+
 }
