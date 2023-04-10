16e21a3918edd7eb0915bea3de9907006ea0dfee
wallaceleonel <wallace.leonell@outlook.com>
2023-04-10 13:27:12 -0300

    refactor: implementando funções.

--- alura/varible.go
+++ alura/varible.go
@@ -1,32 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Gonewson"
-	idade := 9
-	versao := 1.1
 
-	fmt.Println("ola sr.", nome, "cujo idade é ", idade)
-	fmt.Println("Esse programa está na versão.", versao)
-
-	fmt.Println("1- Iniciar monitoramento")
-	fmt.Println("2- Exibir logs")
-	fmt.Println("0- Sair do programa")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi ", comando)
-
-	//if comando == 1 {
-	//	fmt.Println("iniciando monitoramento.")
-	//} else if comando == 2 {
-	//	fmt.Println("Carregando os logs da aplicação.")
-	//} else if comando == 0 {
-	//	fmt.Println("Finalizando programa.")
-	//} else {
-	//	fmt.Println("comando executado invalido.")
-	//}
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	switch comando {
 
@@ -36,8 +19,33 @@ func main() {
 		fmt.Println("Carregando os logs da aplicação.")
 	case 0:
 		fmt.Println("Finalizando programa.")
+		os.Exit(0)
 
 	default:
 		fmt.Println("comando executado invalido.")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Gonewson"
+	idade := 9
+	versao := 1.1
+
+	fmt.Println("ola sr.", nome, "cujo idade é ", idade)
+	fmt.Println("Esse programa está na versão.", versao)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	return comandoLido
+}
+
+func exibeMenu() {
+
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
 }
