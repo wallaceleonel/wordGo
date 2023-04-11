929191894d953357dcbac0c783de70ca4eb7ec05
wallaceleonel <wallace.leonell@outlook.com>
2023-04-11 16:46:52 -0300

    feat: adicionando novo recurso.

--- projetos/bank/main.go
+++ projetos/bank/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ContaCorrente struct {
 	titular             string
@@ -30,12 +32,37 @@ func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	}
 }
 
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+
+	if valorTransferencia > 0 && valorTransferencia < c.saldoConta {
+		c.saldoConta -= valorTransferencia
+		contaDestino.Depositar(valorTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 
 	contaDoJones := ContaCorrente{}
 	contaDoJones.titular = "Jones Manoel"
-	contaDoJones.saldoConta = 300.98
+	contaDoJones.saldoConta = 600.98
+
+	contaDoManoel := ContaCorrente{}
+	contaDoManoel.titular = "Manoel Jones"
+	contaDoManoel.saldoConta = 300.98
 
 	fmt.Println(contaDoJones.Depositar(150.44))
 	fmt.Println(contaDoJones.Sacar(100.98))
+
+	fmt.Println(contaDoManoel.Depositar(150.44))
+	fmt.Println(contaDoManoel.Sacar(100.98))
+
+	status := contaDoJones.Transferir(50.55, &contaDoManoel)
+
+	fmt.Println(status)
+	fmt.Print(contaDoJones.saldoConta)
+	fmt.Print(contaDoManoel.saldoConta)
+
 }
