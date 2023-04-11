3e80055b3f93225e879d64811b93b92540cb6c3c
wallaceleonel <wallace.leonell@outlook.com>
2023-04-11 16:57:23 -0300

    refactor: alterando estrura para modos de pacote.

---
+++ projetos/bank/account/contaCorrente.go
@@ -0,0 +1,40 @@
+package account
+
+type ContaCorrente struct {
+	Titular             string
+	NumeroAgencia       int
+	NumeroContaCorrente int
+	SaldoConta          float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+
+	validaSaque := valorDoSaque > 0 && valorDoSaque <= c.SaldoConta
+	if validaSaque {
+		c.SaldoConta -= valorDoSaque
+		return "Saque realizado com sucesso", c.SaldoConta
+	} else {
+		return " Saldo insuficiente", c.SaldoConta
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	validaDeposito := valorDoDeposito > 0
+	if validaDeposito {
+		c.SaldoConta += valorDoDeposito
+		return "Deposito realizado com sucesso", c.SaldoConta
+	} else {
+		return "Valor de deposito invalido", c.SaldoConta
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+
+	if valorTransferencia > 0 && valorTransferencia < c.SaldoConta {
+		c.SaldoConta -= valorTransferencia
+		contaDestino.Depositar(valorTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
--- projetos/bank/main.go
+++ projetos/bank/main.go
@@ -1,57 +1,19 @@
 package main
 
 import (
+	"C:\.git\wordGo\projetos\bank\account"
 	"fmt"
 )
 
-type ContaCorrente struct {
-	titular             string
-	numeroAgencia       int
-	numeroContaCorrente int
-	saldoConta          float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
-
-	validaSaque := valorDoSaque > 0 && valorDoSaque <= c.saldoConta
-	if validaSaque {
-		c.saldoConta -= valorDoSaque
-		return "Saque realizado com sucesso", c.saldoConta
-	} else {
-		return " Saldo insuficiente", c.saldoConta
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	validaDeposito := valorDoDeposito > 0
-	if validaDeposito {
-		c.saldoConta += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldoConta
-	} else {
-		return "Valor de deposito invalido", c.saldoConta
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
-
-	if valorTransferencia > 0 && valorTransferencia < c.saldoConta {
-		c.saldoConta -= valorTransferencia
-		contaDestino.Depositar(valorTransferencia)
-		return true
-	} else {
-		return false
-	}
-}
-
 func main() {
 
-	contaDoJones := ContaCorrente{}
-	contaDoJones.titular = "Jones Manoel"
-	contaDoJones.saldoConta = 600.98
+	contaDoJones := account.ContaCorrente{}
+	contaDoJones.Titular = "Jones Manoel"
+	contaDoJones.SaldoConta = 600.98
 
-	contaDoManoel := ContaCorrente{}
-	contaDoManoel.titular = "Manoel Jones"
-	contaDoManoel.saldoConta = 300.98
+	contaDoManoel := account.ContaCorrente{}
+	contaDoManoel.Titular = "Manoel Jones"
+	contaDoManoel.SaldoConta = 300.98
 
 	fmt.Println(contaDoJones.Depositar(150.44))
 	fmt.Println(contaDoJones.Sacar(100.98))
@@ -62,7 +24,7 @@ func main() {
 	status := contaDoJones.Transferir(50.55, &contaDoManoel)
 
 	fmt.Println(status)
-	fmt.Print(contaDoJones.saldoConta)
-	fmt.Print(contaDoManoel.saldoConta)
+	fmt.Print(contaDoJones.SaldoConta)
+	fmt.Print(contaDoManoel.SaldoConta)
 
 }
